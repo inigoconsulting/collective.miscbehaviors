3dd7ade1d6f7a348208ec1385bced0ef6fac7143
Izhar Firdaus <kagesenshi.87@gmail.com>
2011-04-23 00:20:06 +0000

    collective.miscbehaviors:  * return None if attribute doesnt exist
    git-svn-id: https://svn.plone.org/svn/collective/collective.miscbehaviors/trunk@238036 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/miscbehaviors/behavior/utils.py
+++ collective/miscbehaviors/behavior/utils.py
@@ -1,6 +1,6 @@
 def context_property(name):
     def getter(self):
-        return getattr(self.context, name)
+        return getattr(self.context, name, None)
     def setter(self, value):
         setattr(self.context, name, value)
     def deleter(self):
