2b6c4fed42cb9c5725e57ddf8525ee625d4bf1bc
Izhar Firdaus <kagesenshi.87@gmail.com>
2011-12-19 09:52:18 +0000

    collective.miscbehaviors:  * enable marker interfaces  * add image portlet for displaying leadimage
    git-svn-id: https://svn.plone.org/svn/collective/collective.miscbehaviors/trunk@247436 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ collective/miscbehaviors/portlets/imageportlet.py
@@ -0,0 +1,53 @@
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.app.portlets.portlets import base
+from zope.formlib import form
+
+from zope.interface import implements
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+from zope import schema
+from collective.miscbehaviors.behavior.leadimage import ILeadImage
+
+class IImagePortlet(IPortletDataProvider):
+    width = schema.Int(title=u'Width')
+    height = schema.Int(title=u'Height')
+
+
+class Assignment(base.Assignment):
+    """Portlet assignment."""
+
+    implements(IImagePortlet)
+
+    def __init__(self, width=150, height=150):
+        super(Assignment, self).__init__()
+        self.width = width
+        self.height = height
+
+    @property
+    def title(self):
+        return u"Lead Image Behavior Portlet"
+
+
+class AddForm(base.AddForm):
+    form_fields = form.Fields(IImagePortlet)
+    label = u'Add Lead Image Behavior Portlet'
+    description = u'This portlet displays lead image of the current content'
+
+    def create(self, data):
+        return Assignment(**data)
+
+class EditForm(base.EditForm):
+    form_fields = form.Fields(IImagePortlet)
+    label = u'Edit Lead Image Behavior Portlet'
+    description = u'This portlet displays lead image of the current content'
+
+
+class Renderer(base.Renderer):
+    """ Portlet renderer. """
+    render = ViewPageTemplateFile("imageportlet.pt")
+
+    @property
+    def available(self):
+        if ILeadImage.providedBy(self.context):
+            return True
+        return False
