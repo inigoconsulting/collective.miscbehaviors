ef9f2505f07c61bf6c54f59a60213f8d2fda6f13
Izhar Firdaus <izhar@inigo-tech.com>
2012-03-11 03:41:24 +0800

    added startenddate behaviors

--- collective/miscbehaviors/indexer.py
+++ collective/miscbehaviors/indexer.py
@@ -1,6 +1,16 @@
 from plone.indexer import indexer
 from collective.miscbehaviors.behavior.bodytext import IBodyText
+from collective.miscbehaviors.behavior.startenddate import IStartEndDate
 
 @indexer(IBodyText)
 def bodytext(obj):
     return obj.text
+
+@indexer(IStartEndDate)
+def startDate(obj):
+    return obj.startDate
+
+@indexer(IStartEndDate)
+def endDate(obj):
+    return obj.endDate
+
