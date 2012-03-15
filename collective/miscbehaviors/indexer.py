48698f3c383b75acb9f47bc17dbf729f013c3906
Izhar Firdaus <izhar@inigo-tech.com>
2012-03-15 11:50:29 +0800

    fix conflict between IBodyText indexer and IDexterityTextIndexer

--- collective/miscbehaviors/indexer.py
+++ collective/miscbehaviors/indexer.py
@@ -2,10 +2,6 @@ from plone.indexer import indexer
 from collective.miscbehaviors.behavior.bodytext import IBodyText
 from collective.miscbehaviors.behavior.startenddate import IStartEndDate
 
-@indexer(IBodyText)
-def bodytext(obj):
-    return obj.text
-
 @indexer(IStartEndDate)
 def startDate(obj):
     return obj.startDate
