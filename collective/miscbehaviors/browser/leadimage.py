2f4b2aba922696bb2e310e51fde787e7b355a357
Izhar Firdaus <izhar@inigo-tech.com>
2013-01-08 17:50:26 +0800

    added default leadimage viewlet

---
+++ collective/miscbehaviors/browser/leadimage.py
@@ -0,0 +1,6 @@
+from plone.app.layout.viewlets.common import ViewletBase
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from collective.miscbehaviors.behavior.leadimage import ILeadImage
+
+class LeadImageViewlet(ViewletBase):
+   pass 
